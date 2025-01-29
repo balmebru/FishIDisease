a43000eb0633ee1603c40ce6962fcd3a53657b77
Deerer99 <123101309+Deerer99@users.noreply.github.com>
2025-01-29 20:27:28 +0100

    Create FishIDisease.py

---
+++ FishIDisease.py
@@ -0,0 +1,12 @@
+class FishIDisease:
+    def __init__(self):
+        pass
+
+    def load_video(self, path: str):
+        pass
+
+    def run_pipeline(self):
+        pass
+
+    def export_results(self, output_path: str):
+        pass
