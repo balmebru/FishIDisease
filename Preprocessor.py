baf3721c9c0a697e70c95fdd9d904e4d0e06099d
Deerer99 <123101309+Deerer99@users.noreply.github.com>
2025-01-29 20:28:03 +0100

    Create Preprocessor.py

---
+++ Preprocessor.py
@@ -0,0 +1,12 @@
+class Preprocessor:
+    def __init__(self):
+        pass
+
+    def background_subtraction(self, image):
+        pass
+
+    def apply_denoising(self, image):
+        pass
+
+    def lighting_correction(self, image, correction_factors):
+        pass
