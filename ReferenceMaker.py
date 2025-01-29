766b610778a0cbdc2ba241d301bf19d2f60c595d
Deerer99 <123101309+Deerer99@users.noreply.github.com>
2025-01-29 20:28:26 +0100

    Create ReferenceMaker.py

---
+++ ReferenceMaker.py
@@ -0,0 +1,9 @@
+class ReferenceMaker:
+    def __init__(self):
+        pass
+
+    def detect_reference_object(self, image):
+        pass
+
+    def compute_correction_factors(self, reference_object):
+        pass
