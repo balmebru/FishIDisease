9118ebfb635e40e04b81e9e336044c5c662f1cb9
Deerer99 <123101309+Deerer99@users.noreply.github.com>
2025-01-29 20:29:08 +0100

    Create Segmenter.py

---
+++ Segmenter.py
@@ -0,0 +1,12 @@
+class Segmenter:
+    def __init__(self):
+        pass
+
+    def detect_fish(self, image):
+        pass
+
+    def segment_fish(self, image):
+        pass
+
+    def extract_mask(self, segmented_image):
+        pass
