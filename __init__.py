4cbdffb68c7f5b9417cd86b4b3cdbef59336df06
Deerer99 <123101309+Deerer99@users.noreply.github.com>
2025-01-29 20:30:56 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+from .FishIDisease import FishIDisease
+from .Preprocessor import Preprocessor
+from .ReferenceMaker import ReferenceMaker
+from .Segmenter import Segmenter
+from .DiseaseID import DiseaseID
+
+__all__ = [
+    "FishIDisease",
+    "Preprocessor",
+    "ReferenceMaker",
+    "Segmenter",
+    "DiseaseID",
+]
