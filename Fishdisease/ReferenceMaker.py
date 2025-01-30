6669f8597442a3753041146de4dc2f14a4d52131
Chirumamilla Hagadur <saikeerthi.chirumamillahagadur@siemens-healthineers.com>
2025-01-30 13:33:31 +0100

    computing and applying brightness & color factors

--- Fishdisease/ReferenceMaker.py
+++ Fishdisease/ReferenceMaker.py
@@ -1,9 +1,67 @@
+import cv2
+import numpy as np
+from ultralytics import YOLO
+from Segmenter import Segmenter
+
+
 class ReferenceMaker:
-    def __init__(self):
+    def __init__(self, model_path):
+        """"
+        Initializing the model
+        """
+        self.model = YOLO(model_path)
         pass
 
     def detect_reference_object(self, image):
-        pass
+        """
+        Detect and segment the reference object in an image
+        
+        """
+        mask = self.segment_fish.detect_fish(image)  
+        if mask is None:
+            return None, None
+        return mask
 
-    def compute_correction_factors(self, reference_object):
-        pass
+    def compute_correction_factors(self, image, reference_mask):
+        """
+        Computing brightness and colr correction factors
+        """
+        ref_region = cv2.bitwise_and(image,image,mask= reference_mask)
+        ## brightness
+        avg_b = np.mean(ref_region)
+        target_b = 128 # normalizing to mid
+        b_factor = target_b / avg_b
+
+        ## color
+
+        mean_r, mean_g, mean_b = cv2.mean(ref_region)
+        target_r, target_g, target_b = 128, 128, 128
+        r_factor, g_factor, b_factor = target_r / mean_r, target_g / mean_g, target_b / mean_b
+
+        return b_factor, (r_factor, g_factor, b_factor)
+    
+
+    def apply(self, image, b_factor, c_factors):
+
+        """
+        applying the computed factors to image
+        """
+        r_factor, g_factor, b_factor = c_factors
+        corrected = cv2.merge([
+            np.clip(image[..., 0] * b_factor, 0, 255).astype(np.uint8),
+            np.clip(image[..., 1] * g_factor, 0, 255).astype(np.uint8),
+            np.clip(image[..., 2] * r_factor, 0, 255).astype(np.uint8)
+
+        ])
+        adjusted = np.clip(corrected * b_factor, 0, 255).astype(np.uint8)
+        return adjusted
+    
+    def resize_to_reference(self, image, ref_bbox, target_size=(100, 100)):
+        """
+        Resizing the reference object to standard size.
+        """
+        x1, y1, x2, y2 = map(int, ref_bbox)
+        ref_object = image[y1:y2, x1:x2]
+        resized = cv2.resize(ref_object, target_size, interpolation=cv2.INTER_LINEAR)
+        return resized
+
